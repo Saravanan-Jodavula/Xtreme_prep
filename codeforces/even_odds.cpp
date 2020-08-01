899872a723889112c38dd116ca836c7fea705b8d
Saravanan <jvs.ravanan@gmail.com>
2020-08-02 01:02:13 +0530

    6

--- codeforces/even_odds.cpp
+++ codeforces/even_odds.cpp
@@ -2,13 +2,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    long int n,k;
-    vector<long int> v;
+    long long int n(0),k(0), p(0);
     cin>>n>>k;
-    for(long int i=1;i<=n;i=i+2)
-        v.push_back(i);
-    for(long int j=2;j<=n;j=j+2)
-        v.push_back(j);
-    cout<<v[k-1]<<endl;
-    return 0;
+    if(k%2 == 0 && k==n && k!=1){
+        cout<<k<<endl;
+        return 0;
+    }
+    else if(k%2 != 0 && k==n && k!=1){
+        cout<<k-1<<endl;
+        return 0;
+    }
+
+        if(k > (n+1)/2){
+            if(n%2==0)
+            p = k - (n/2);
+            else
+            p = k - ((n+1)/2);
+            cout<<2*(p)<<endl;
+            return 0;
+        }
+        else{
+            cout<<(2*k-1)<<endl;
+            return 0;
+        }
 }
